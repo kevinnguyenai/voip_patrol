636ccd166a1670d0e57d2b28cdb3eb34115d019e
KevinAtLeap <tuan.anh.nguyen@leap.expert>
2023-07-03 17:10:41 +0700

    still not yet success

--- docker/build_image_arm.sh
+++ docker/build_image_arm.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 TAG="0.4.0"
-PLATFORM="linux/arm64/v8"
+PLATFORM="linux/arm64"
 docker build -f Dockerfile.arm . --no-cache --platform ${PLATFORM} -t voip_patrol
 docker tag voip_patrol_arm:latest ocpasia/voip_patrol_arm:latest
 docker tag voip_patrol_arm:latest ocpasia/voip_patrol_arm:${TAG}
