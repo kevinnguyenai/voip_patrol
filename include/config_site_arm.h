d782d8423650fffa90bea6fe8b531870c7fd19eb
KevinAtLeap <tuan.anh.nguyen@leap.expert>
2023-07-04 09:05:53 +0700

    autobuild

--- include/config_site_arm.h
+++ include/config_site_arm.h
@@ -2,6 +2,7 @@
 // #define FD_SETSIZE PJ_IOQUEUE_MAX_HANDLES
 //
 //
+#define PJ_AUTOCONF 1
 #define PJ_IOQUEUE_MAX_HANDLES      1024
 #define FD_SETSIZE_SETABLE      1
 #define __FD_SETSIZE            1024
@@ -18,9 +19,9 @@
 // Make send of "100 - Trying" explicit
 #define PJSUA_DISABLE_AUTO_SEND_100 1
 // ARM
-#define PJ_HAS_PENTIUM	0
-#define PJ_IS_LITTLE_ENDIAN	0
-#define PJ_IS_BIG_ENDIAN	1
+//#define PJ_HAS_PENTIUM	0
+//#define PJ_IS_LITTLE_ENDIAN	0
+//#define PJ_IS_BIG_ENDIAN	1
 
 /// MACOS_X ARM
 //#define PJ_DARWINOS 1
