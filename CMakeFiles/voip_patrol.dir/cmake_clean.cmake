d6034f2f56fa8927a6aa21a3edc72fdfafdeedba
KevinAtLeap <tuan.anh.nguyen@leap.expert>
2023-07-22 22:27:31 +0700

    feature/mac

---
+++ CMakeFiles/voip_patrol.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/voip_patrol.dir/src/ezxml/ezxml.c.o"
+  "CMakeFiles/voip_patrol.dir/src/ezxml/ezxml.c.o.d"
+  "CMakeFiles/voip_patrol.dir/src/voip_patrol/action.cc.o"
+  "CMakeFiles/voip_patrol.dir/src/voip_patrol/action.cc.o.d"
+  "CMakeFiles/voip_patrol.dir/src/voip_patrol/check.cc.o"
+  "CMakeFiles/voip_patrol.dir/src/voip_patrol/check.cc.o.d"
+  "CMakeFiles/voip_patrol.dir/src/voip_patrol/mod_voip_patrol.cc.o"
+  "CMakeFiles/voip_patrol.dir/src/voip_patrol/mod_voip_patrol.cc.o.d"
+  "CMakeFiles/voip_patrol.dir/src/voip_patrol/voip_patrol.cc.o"
+  "CMakeFiles/voip_patrol.dir/src/voip_patrol/voip_patrol.cc.o.d"
+  "voip_patrol"
+  "voip_patrol.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/voip_patrol.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/voip_patrol.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for voip_patrol.
+# This may be replaced when dependencies are built.
