9feed71af5f84d17cc4b13a5de159333fd698192
KevinAtLeap <tuan.anh.nguyen@leap.expert>
2023-07-23 11:06:22 +0700

    feature/mac

--- include/config_site_arm.h
+++ include/config_site_arm.h
@@ -1,8 +1,6 @@
-// #define PJ_IOQUEUE_MAX_HANDLES 1024
-// #define FD_SETSIZE PJ_IOQUEUE_MAX_HANDLES
-//
-//
+#include "config_site_sample.h"
 //#define PJ_AUTOCONF 1
+#define PJ_CONFIG_IPHONE 1
 #define PJ_IOQUEUE_MAX_HANDLES      1024
 #define FD_SETSIZE_SETABLE      1
 #define __FD_SETSIZE            1024
@@ -19,10 +17,10 @@
 // Make send of "100 - Trying" explicit
 #define PJSUA_DISABLE_AUTO_SEND_100 1
 // ARM
-#define ARM 1
-#define PJ_HAS_PENTIUM	0
-#define PJ_IS_LITTLE_ENDIAN	1
-#define PJ_IS_BIG_ENDIAN	0
+//#define ARM 1
+//#define PJ_HAS_PENTIUM	0
+//#define PJ_IS_LITTLE_ENDIAN	1
+//#define PJ_IS_BIG_ENDIAN	0
 
-/// MACOS_X ARM
+// MACOS_X ARM
 //#define PJ_DARWINOS 1
