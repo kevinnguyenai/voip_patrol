59f45cad864526bf2e8f0a1d9e992cc249deae68
kevinnguyenai <kevin.nguyen.ai@gmail.com>
2023-07-24 15:31:36 +0700

    changed mac

--- load_test/run.sh
+++ load_test/run.sh
@@ -35,16 +35,25 @@ run_voip_patrol () {
 	tmux send-keys "${ENVS} bash -c \"${CMD}\"" Enter
 }
 
+run_voip_patrol_new () {
+	P=$((${ID}+1))
+	DELAY=$(((${ID}-1)*2))
+	ENVS="VP_ENV_SERVER=\"${SERVER}\" VP_ENV_CALLEE=\"*98@35.179.77.242:5060\" VP_ENV_U=demo_000009 "
+	CMD="sleep ${DELAY} && ../voip_patrol -p ${P}000 -c load.xml -o perf${ID}.json --rtp-port ${ID}0010"
+	# echo "$ENVS bash -c \"$CMD\""
+	tmux send-keys "${ENVS} bash -c \"${CMD}\"" Enter
+}
+
 # start a voip_patrol
 tmux select-pane -t 0
 ID=1
-run_voip_patrol
+run_voip_patrol_new
 tmux split-window -v
 
 # start a voip_patrol
 tmux select-pane -t 1
 ID=2
-run_voip_patrol
+run_voip_patrol_new
 tmux select-pane -t 2
 tmux send-keys "htop" C-m
 tmux split-window -v
@@ -52,13 +61,13 @@ tmux split-window -v
 # start a voip_patrol
 tmux select-pane -t 3
 ID=3
-run_voip_patrol
+run_voip_patrol_new
 tmux split-window -v
 
 # start a voip_patrol
 tmux select-pane -t 4
 ID=4
-run_voip_patrol
+run_voip_patrol_new
 
 # Set default window
 tmux select-window -t $SESSION:3
@@ -67,3 +76,7 @@ tmux select-window -t $SESSION:3
 tmux -2 attach-session -t $SESSION
 
 echo "grep \"action\\\": \\\"call\\\"\" --no-filename  perf[1-4].json | jq . | grep mos_lq | sort | uniq -c"
+
+#example 
+#VP_ENV_SERVER="35.179.77.242:5060" VP_ENV_CALLEE="demo_000001@35.179.77.242:5060" VP_ENV_U=demo_000001  bash -c "sleep 0 && ../voip_patrol -p 2000 -c load.xml -o perf1.json --rtp-port 10010" 
+#VP_ENV_SERVER="35.179.77.242" VP_ENV_CALLEE="001252424631@35.179.77.242:5060" VP_ENV_U=demo_000009  bash -c "sleep 0 && ../voip_patrol -p 32579 -c load.xml -o perf1.json --rtp-port 10010" 
